dc7b151b3369eb3c8da7fd34fb5e92de01f1739f
unknown <choongang501@choongang>
2019-10-26 13:24:15 +0900

    variable

---
+++ src/chap02/Exercises.java
@@ -0,0 +1,189 @@
+package chap02;
+
+public class Exercises {
+	public static void main(String[] args) {
+		// 1. 답 4
+		int var = 3;
+		var = 4;
+//		var = 4.3;
+		
+		if (var > 5) {
+			int var2;
+			var2 = 7;
+			
+		}
+		
+//		var2 = 8;
+		
+		// 2. 
+		int modelName;
+		int $value;
+		int _age;
+
+		
+		// 3. 
+		// 정수 byte(1byte), short;char(2byte), int(4byte),   long(8byte)
+		// 실수                               float(4byte), double(8byte)
+		// 논리 boolean(1byte)
+		
+		
+		// 4. 
+		// 타입 : int, double
+		// 변수이름 : age, price
+		// 리터럴 : 10, 3.14
+		
+		
+		// 5. 답3
+		byte byteValue = 10;
+		char charValue = 'A';
+		
+		int intValue1 = byteValue;
+		int intValue2 = charValue;
+//		short shortValue = charValue;
+		double doubleValue = byteValue;
+		
+
+		// 6. 답4
+		int intValue6 = 10;
+		char charValue6 = 'A';
+		double doubleValue6 = 5.7;
+		String strValue = "A";
+		
+		double var61 = (double) intValue6;
+		byte var62 = (byte) intValue6;
+		int var63 = (int) doubleValue6;
+//		char var = (char) strValue;
+
+		
+		// 7. 답3
+		int var1 = 10;
+		long var2 = 10000000000000000L;
+//		char var3 = '';
+		double var4 = 10;
+		float var5 = 10;
+
+		
+		// 8.
+		byte byteValue8 = 10;
+		float floatValue8 = 2.5F;
+		double doubleValue8 = 2.5;
+		
+//		byte result1 = byteValue8 + byteValue8;
+		int result2 = 5 + byteValue8;
+		float result3 = 5 + floatValue8;
+		double result4 = 5 + doubleValue8;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
