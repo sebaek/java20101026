0c2740e810540605a0430ae4df50319dacbe5676
unknown <choongang501@choongang>
2019-11-02 11:59:03 +0900

    chap04 exer06

---
+++ src/chap04/Exercise06.java
@@ -0,0 +1,60 @@
+package chap04;
+
+public class Exercise06 {
+	public static void main(String[] args) {
+		System.out.println("*");
+		System.out.println("**");
+		System.out.println("***");
+		System.out.println("****");
+		System.out.println("*****");
+		
+		System.out.print("*");
+		System.out.println();
+		
+		System.out.print("*");
+		System.out.print("*");
+		System.out.println();
+		
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.println();
+		
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.println();
+		
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.println();
+		
+		
+		
+
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 0; j < i; j++) {
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+		
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
