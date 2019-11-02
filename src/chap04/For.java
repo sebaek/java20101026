1e362d80aad270f6f959e3c52dd9cf1e33985e70
unknown <choongang501@choongang>
2019-11-02 11:09:55 +0900

    for

---
+++ src/chap04/For.java
@@ -0,0 +1,47 @@
+package chap04;
+
+public class For {
+	public static void main(String[] args) {
+		// for
+
+		// 반복할 명령문이 있을 때 사용
+//		System.out.println(1);
+//		System.out.println(2);
+//		System.out.println(3);
+//		System.out.println(4);
+//		System.out.println(5);
+//		System.out.println(6);
+//		System.out.println(7);
+//		System.out.println(8);
+//		System.out.println(9);
+//		System.out.println(10);
+
+//		for ( 1 초기화 ;  2 조건식 ; 4 증감식) {
+//			3 실행문;
+//		}
+// 		실행순서 1, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4
+
+		for (int i = 1; i <= 10; i++) {
+
+			System.out.println(i);
+
+		}
+
+		for (int i = 10; i >= 1; i--) {
+
+			System.out.println(i);
+
+		}
+
+
+
+	}
+}
+
+
+
+
+
+
+
+
