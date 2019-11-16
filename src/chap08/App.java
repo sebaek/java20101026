238e06b964947a69619190821eed0bd9a7e7ffba
unknown <choongang501@choongang>
2019-11-16 10:14:17 +0900

    chap08

--- src/chap08/App.java
+++ src/chap08/App.java
@@ -2,14 +2,24 @@ package chap08;
 
 public class App {
 	public static void main(String[] args) {
-		Pet pet1 = new Dog();
+		Dog pet1 = new Dog();
 		Pet pet2 = new Cat();
 		
 		play(pet1);
 		play(pet2);
+		
+		Pet p = pet1;
+		Guard g = pet1;
+		
+		guard(pet1);
+		guard(g);
 	}
 	
 	public static void play(Pet pet) {
 		pet.sit();
+	}
+	
+	public static void guard(Guard guard) {
+		guard.stop();
 	}
 }
--- src/chap08/Dog.java
+++ src/chap08/Dog.java
@@ -1,8 +1,14 @@
 package chap08;
 
-public class Dog implements Pet {
+public class Dog implements Pet, Guard {
 	@Override
 	public void sit() {
 		System.out.println("앉아서 웃습니다.");
+	}
+	
+	@Override
+	public void stop() {
+		System.out.println("낯선사람에게 짖습니다.");
+		
 	}
 }
